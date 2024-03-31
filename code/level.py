90beacf0d2c0c1a9d0653da82b018708716d450a
valod911 <wowan92bbb@gmail.com>
2024-04-01 01:21:27 +0200

    Add the gravity; Add the horizontal collisions;

--- code/level.py
+++ code/level.py
@@ -8,17 +8,18 @@ class Level:
 
         # groups
         self.all_sprites = pygame.sprite.Group()
+        self.collision_sprites = pygame.sprite.Group()
 
         self.setup(tmx_map)
 
     def setup(self, tmx_map):
         for x,y,surf in tmx_map.get_layer_by_name('Terrain').tiles():
-            Sprite((x * TILE_SIZE,y * TILE_SIZE), surf, self.all_sprites)
+            Sprite((x * TILE_SIZE,y * TILE_SIZE), surf, (self.all_sprites, self.collision_sprites))
 
 
         for obj in tmx_map.get_layer_by_name('Objects'):
             if obj.name == 'Player':
-                Player((obj.x, obj.y), self.all_sprites)
+                Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
                 print(obj.x, obj.y)
 
     def run(self, dt):
--- code/player.py
+++ code/player.py
@@ -3,15 +3,22 @@ import pygame
 from settings import *
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, groups):
+    def __init__(self, pos, groups, collision_sprites):
         super().__init__(groups)
         self.image = pygame.Surface((48,56))
         self.image.fill('red')
+
+        # rects
         self.rect = self.image.get_frect(topleft=pos)
+        self.old_rect = self.rect.copy()
 
         # movement
         self.direction = vector(0,0)
         self.speed = 200
+        self.gravity = 1300
+
+        # collisions
+        self.collision_sprites = collision_sprites
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -20,11 +27,32 @@ class Player(pygame.sprite.Sprite):
             input_vector.x += 1
         if keys[pygame.K_LEFT]:
             input_vector.x -= 1
-        self.direction = input_vector.normalize() if input_vector else input_vector
+        self.direction.x = input_vector.normalize().x if input_vector else 0
 
     def move(self, dt):
-        self.rect.topleft += self.direction * self.speed * dt
+        # horizontal
+        self.rect.x += self.direction.x * self.speed * dt
+        self.collision('horizontal')
 
+        # vertical
+        self.direction.y += self.gravity/2 * dt
+        self.rect.y += self.direction.y * dt
+        self.direction.y += self.gravity/2 * dt
+        self.collision('vertical')
+
+    def collision(self, axis):
+        for sprite in self.collision_sprites:
+            if sprite.rect.colliderect(self.rect):
+                if axis == 'horizontal':
+                    # left
+                    if self.rect.left <= sprite.rect.right and self.old_rect.left >= sprite.old_rect.right:
+                        self.rect.left = sprite.rect.right
+                    # right
+                    if self.rect.right >= sprite.rect.left and self.old_rect.right <= sprite.old_rect.left:
+                        self.rect.right = sprite.rect.left
+                else:   # vertical
+                    pass
     def update(self, dt):
+        self.old_rect = self.rect.copy()
         self.input()
         self.move(dt)
